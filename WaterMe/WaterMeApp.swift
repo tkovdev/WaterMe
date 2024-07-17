0be08a1b50b9e82486046e7ee1447902c9d68d47
tkov.dev <tommy.pk17@gmail.com>
2024-07-17 19:20:36 +0200

    Initial Commit

---
+++ WaterMe/WaterMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterMeApp.swift
+//  WaterMe
+//
+//  Created by Tommy Kovalchuk on 2024-07-17.
+//
+
+import SwiftUI
+
+@main
+struct WaterMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
