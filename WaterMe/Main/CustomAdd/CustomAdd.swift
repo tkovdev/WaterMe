caff64e283cf71ec6e70be9fdd0e94effbba5d8a
tkov.dev <tommy.pk17@gmail.com>
2025-01-02 08:20:40 -0500

    custom add function

---
+++ WaterMe/Main/CustomAdd/CustomAdd.swift
@@ -0,0 +1,18 @@
+//
+//  CustomAdd.swift
+//  WaterMe
+//
+//  Created by Tommy Kovalchuk on 2024-12-29.
+//
+
+import SwiftUI
+
+struct CustomAdd: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomAdd()
+}
